11a33763626ed85ca53ea68b7f9a43edd114a991
Sergei Lebedev <superbobry@gmail.com>
2017-06-04 12:50:48 +0200

    Try the CMake-based build for JVM packages

--- install_xgboost.py
+++ install_xgboost.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, unicode_literals
 
+import glob
 import os
 import re
 import shutil
@@ -21,6 +22,9 @@ if __name__ == "__main__":
     shutil.copy(os.path.join("..", "config_" + sys.platform + ".mk"),
                 "config.mk")
 
+    # HACK: library name was changed in the latest version.
+    sed_inplace("CMakeLists.txt", "dmlccore", "dmlc")
+
     os.chdir("jvm-packages")
     run("mvn -q -B versions:set -DnewVersion=" + os.environ["XGBOOST_VERSION"])
 
@@ -37,8 +41,45 @@ if __name__ == "__main__":
         "-DnewVersion=[{}]".format(os.environ["SPARK_VERSION"]))
 
     if sys.platform in ["cygwin", "win32"]:
+        # Remove once https://github.com/dmlc/xgboost/pull/2379 is merged.
+        sed_inplace("xgboost4j/src/native/xgboost4j.cpp",
+                    "jint JNI_OnLoad",
+                    "JNIEXPORT jint JNICALL JNI_OnLoad")
+        sed_inplace("xgboost4j/src/native/xgboost4j.cpp",
+                    "cbatch.offset = reinterpret_cast<long *>",
+                    "cbatch.offset = reinterpret_cast<jlong *>")
+
         with cd(".."):
-            run("mingw32-make jvm")
+            maybe_makedirs("build")
+            with cd("build"):
+                run("cmake .. -DJVM_BINDINGS:BOOL=ON -DUSE_OPENMP:BOOL=ON")
+                run("cmake --build . --target ALL_BUILD")
+
+        # Back to jvm-packages.
+        resources_dir = os.path.join("xgboost4j", "src", "main", "resources")
+        shutil.copytree(os.path.join("..", "lib"), resources_dir)
+
+        # Copy Python to native resources.
+        shutil.copy(os.path.join("..", "dmlc-core", "tracker",
+                                 "dmlc_tracker", "tracker.py"),
+                    resources_dir)
+
+        # Copy test data files
+        maybe_makedirs(
+            os.path.join("xgboost4j-spark", "src", "test", "resources"))
+        with cd(os.path.join("..", "demo", "regression")):
+            run("{} mapfeat.py".format(sys.executable))
+            run("{} mknfold.py machine.txt 1".format(sys.executable))
+
+        for file in glob.glob(
+                os.path.join("..", "demo", "regression", "machine.txt.t*")):
+            shutil.copy(file, "xgboost4j-spark/src/test/resources")
+        for file in glob.glob(
+                os.path.join("..", "demo", "data", "agaricus.*")):
+            shutil.copy(file, "xgboost4j-spark/src/test/resources")
+
+        with open("create_jni.bat", "w"):
+            pass  # Erase. We've done all the work.
 
     run("mvn -q install -pl :xgboost4j,:xgboost4j-spark "
         "-DskipTests -Dmaven.test.skip",
