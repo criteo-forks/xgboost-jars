39498a562d01f089bca3c0b161010f46d9ffd01e
Sergei Lebedev <s.lebedev@criteo.com>
2017-06-21 17:24:50 +0200

    First take on Docker-powered Linux builds
    to overcome the nasty newer libc issue on CentOS6.

---
+++ travis/docker.sh
@@ -0,0 +1,43 @@
+set -xe
+
+# Clean the yum cache
+yum -y clean all
+yum -y clean expire-cache
+
+# Install all the needed packages.
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+
+yum -y install wget tar cmake3 zlib-devel \
+    python34 java-1.8.0-openjdk-devel maven
+
+# Setup SCL for newer GCC.
+yum -y install centos-release-scl-rh
+yum -y install devtoolset-4-{gcc,gcc-c++}
+
+source /opt/rh/devtoolset-4/enable
+export CC=/opt/rh/devtoolset-4/root/usr/bin/gcc
+export CXX=/opt/rh/devtoolset-4/root/usr/bin/c++
+
+cd /xgboost-jars
+
+# No Maven on CentOS6.
+wget -qc http://mirrors.ircam.fr/pub/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
+tar -zxf apache-maven-3.5.0-bin.tar.gz
+export PATH=$PWD/apache-maven-3.5.0/bin:$PATH
+
+# CMake3 is not aliased to ``cmake``
+ln -s /usr/bin/cmake3 /usr/bin/cmake
+rm /usr/bin/python
+ln -s /usr/bin/python3 /usr/bin/python
+
+export JAVA_HOME=/usr/lib/jvm/java
+
+# For some reason CMake wouldn't pick up the compilers from devtoolset.
+# Point it to them manually.
+mkdir -p xgboost/build
+pushd xgboost/build
+cmake .. -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX
+popd
+
+sh travis/install.sh
+sh travis/test_and_package.sh
---
+++ travis/test_and_package.sh
@@ -0,0 +1,9 @@
+set -xe
+
+cd xgboost/jvm-packages
+mvn -q package -pl :xgboost4j,:xgboost4j-spark
+
+mv xgboost4j/target/xgboost4j-$XGBOOST_VERSION.jar \
+   $TRAVIS_BUILD_DIR/xgboost4j-$XGBOOST_VERSION-$TRAVIS_OS_NAME.jar
+mv xgboost4j-spark/target/xgboost4j-spark-$XGBOOST_VERSION.jar \
+   $TRAVIS_BUILD_DIR/xgboost4j-spark-$XGBOOST_VERSION-$TRAVIS_OS_NAME.jar
