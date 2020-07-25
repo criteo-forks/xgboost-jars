a1f134a737d7b32ce6aa6995144bcee74e3d5fbc
Anthony D'Amato <anthony.damato@hotmail.fr>
2020-07-25 23:47:45 +0200

    fix mac build

--- travis/test_and_package.sh
+++ travis/test_and_package.sh
@@ -4,9 +4,9 @@ export HADOOP_HOME=$PWD/xgboost
 export HADOOP_HDFS_HOME=$PWD/xgboost
 
 cd xgboost/jvm-packages
-mvn -q package -pl :xgboost4j,:xgboost4j-spark -Prelease
+mvn -q package -pl xgboost4j,xgboost4j-spark -DskipTests -Dmaven.test.skip
 
-mv xgboost4j/target/xgboost4j-$XGBOOST_VERSION.jar \
-   $TRAVIS_BUILD_DIR/xgboost4j-$XGBOOST_VERSION-$TRAVIS_OS_NAME.jar
-mv xgboost4j-spark/target/xgboost4j-spark-$XGBOOST_VERSION.jar \
-   $TRAVIS_BUILD_DIR/xgboost4j-spark-$XGBOOST_VERSION.jar
+cp xgboost4j/target/xgboost4j_$SCALA_BINARY_VERSION-$XGBOOST_VERSION.jar \
+   $TRAVIS_BUILD_DIR/xgboost4j_$SCALA_BINARY_VERSION-$XGBOOST_VERSION-$TRAVIS_OS_NAME.jar
+cp xgboost4j-spark/target/xgboost4j-spark_$SCALA_BINARY_VERSION-$XGBOOST_VERSION.jar \
+   $TRAVIS_BUILD_DIR/xgboost4j-spark_$SCALA_BINARY_VERSION-$XGBOOST_VERSION.jar
